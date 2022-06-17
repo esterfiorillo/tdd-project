5685122e85f85e28d6d4273e85b0f684c04bdaeb
esterfiorillo <ester.fiorillo01@gmail.com>
2022-06-17 12:18:38 -0300

    De volta ao teste funcional - PARTE 2

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(StaticLiveServerTestCase):
+class NewVisitorTest(FunctionalTest):
 
 
     def test_can_start_a_list_for_one_user(self): 
